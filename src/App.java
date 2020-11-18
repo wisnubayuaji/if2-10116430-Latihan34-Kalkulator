10b1bcb51582976beeb34c2b1a490b1017155550
wisnubayuaji <wisnubayu34@gmail.com>
2020-11-18 23:48:59 +0700

    1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+ 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Kalkulator k = new Kalkulator();
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.println("====Aplikasi Kalkulator Bilangan====");
+        
+        //in
+        System.out.print("Masukan Angka ke-1 : ");
+        double value1 = scn.nextDouble();
+        System.out.print("Masukan Angka ke-2 : ");
+        double value2 = scn.nextDouble();
+        
+        //out
+        System.out.println("Hasil Pertambahan : " +k.tambahBilangan());
+        System.out.println("Hasil Pengurangan : " +k.kurangBilangan());
+        System.out.println("Hasil Perkalian : " +k.kaliBilangan());
+        System.out.println("Hasil Pembagian : " +k.bagiBilangan());
+        System.out.println("Hasil Sisa : " +k.sisaBilangan());
     }
 }
---
+++ src/Kalkulator.java
@@ -0,0 +1,34 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+ 
+ class Kalkulator {
+    double value1;
+    double value2;
+    
+    double tambahBilangan() {
+        double hasil = value1 + value2;
+        return hasil;
+    }
+    double kurangBilangan() {
+        double hasil = value1 - value2;
+        return hasil;
+    }
+    double kaliBilangan() {
+        double hasil = value1 * value2;
+        return hasil;
+    }
+    double bagiBilangan() {
+        double hasil = value1 / value2;
+        return hasil;
+    }
+    double sisaBilangan() {
+        double hasil = value1 % value2;
+        return hasil;
+    }
+    
+    
+}
